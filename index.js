979d509209bd59ee36ffcc9463f7cd18f0f6844b
MateHUN <matehunter123gmail.com>
2020-08-05 14:35:06 +0200

    sdf
    sdf

--- index.js
+++ index.js
@@ -7,74 +7,19 @@ var bot = new commando.Client ({
     disabledEvryone: true,
     unknownCommandResponse: false
 });
-//FILE SYSTEM
-bot.on('message', async message => {
-
-
-    let prefixek = JSON.parse(fs.readFileSync("./prefixek.json", "utf8"));
-    if(!prefixek[message.guild.id]) {
-        prefixek[message.guild.id] = {
-            prefix: botconfig.prefix
-        }
-    }
-    let prefix = prefixek[message.guild.id].prefix;
-
-    //ARGS
-
-    let messageArry = message.content.split(" ");
-    let cmd = messageArry[0];
-    let args = messageArry.slice(1);
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if(commandfile) commandfile.run(bot,message,args)
-
-    //CHECK CHANNEL TYPE
-
-    if(message.channel.type === 'dm') return;
-    if(message.author.bot) return;
-
-    //CHECK PREFIX, DEFINE ARGS & COMMAND
-
-    // RUN COMMANDS 
-
-   if(bot.commands.has(cmd)) {
-       command = bot.commands.get(cmd);
-   } else if (bot.aliases.has(cmd)) {
-       commands = bot.commands.get(bot.aliases.get(cmd));
-   }
+let prefix = '?'
+bot.on('message', async (message) => 
+{
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  
     try {
-        command.run(bot, message, args);
-    } catch(e) {
-        return
-    }
-});
-
-//-------------------------------------
-
-//READ COMMAND FOLDER
-
-fs.readdir("./commands/", (err,files) => {
-    if (err) console.log(err)
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0) {
-        console.log('nem találni ilyen parancsot!')
-        return
-    }
-
-    jsfile.forEach((f) => {
-        let props = require(`./commands/${f}`);
-        console.log(`${f} Betöltve!`)
-        bot.commands.set(props.help.name, props);
-
-
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.name);
-        })
-    })
-})
-
-bot.commands = new discord.Collection();
-bot.aliases = new discord.Collection();
+      let commandFile = require(`./commands/${command}.js`);
+      commandFile.run(bot, message, args, prefix, moment);
+    } catch (err) {
+}});
 
 
 // ---------------------------###Eleje###------------------------------- \\
